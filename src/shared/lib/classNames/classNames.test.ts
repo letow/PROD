650770090d68768de93d678ce6ad17a11ce57081
George <71167099+letow@users.noreply.github.com>
2023-02-13 01:32:45 +0500

    added jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,32 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with only first param", () => {
+    expect(classNames("someClass")).toBe("someClass");
+  });
+
+  test("with additional class", () => {
+    const expected = "someClass cls garfield";
+    expect(classNames("someClass", {}, ["cls", "garfield"])).toBe(expected);
+  });
+
+  test("with mods", () => {
+    const expected = "someClass cls garfield scrollable";
+    expect(
+      classNames("someClass", { scrollable: true, hovered: false }, [
+        "cls",
+        "garfield",
+      ])
+    ).toBe(expected);
+  });
+
+  test("with mod undefined", () => {
+    const expected = "someClass cls garfield scrollable";
+    expect(
+      classNames("someClass", { scrollable: true, hovered: undefined }, [
+        "cls",
+        "garfield",
+      ])
+    ).toBe(expected);
+  });
+});
