165ee3cc263e223d133049aa3465e1e34a4d3887
George <71167099+letow@users.noreply.github.com>
2023-02-18 11:27:43 +0500

    added ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
+
+//Компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+  const [error, setError] = useState(false);
+
+  const onThrow = () => {
+    setError(true);
+  };
+
+  useEffect(() => {
+    if (error) throw new Error();
+  }, [error]);
+  return (
+    <Button theme={ThemeButton.CLEAR} onClick={onThrow}>
+      Throw Error
+    </Button>
+  );
+};
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
+import s from "./PageError.module.scss";
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError = ({ className }: PageErrorProps) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    location.reload();
+  };
+
+  return (
+    <div className={classNames(s.PageError, {}, [className])}>
+      <h1>{t("Ошибка")}</h1>
+      <span></span>
+      <Button theme={ThemeButton.CLEAR} onClick={reloadPage}>
+        {t("Обновить")}
+      </Button>
+    </div>
+  );
+};
