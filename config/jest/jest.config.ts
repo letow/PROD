d91951e1bdfd2a5b85e96073d0613c231c3b4bd0
George <71167099+letow@users.noreply.github.com>
2023-02-18 14:50:25 +0500

    added tests (for Button)

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -70,7 +70,7 @@ export default {
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: { "\\.s?css$": "identity-obj-proxy" },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -117,7 +117,7 @@ export default {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["<rootDir>config/jest/jest-setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -178,4 +178,6 @@ export default {
   // Whether to use watchman for file crawling
   // watchman: true,
   preset: "ts-jest",
+  // for absolute imports to work
+  modulePaths: ["<rootDir>src"],
 };
