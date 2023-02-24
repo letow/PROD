a0861e5e400f87e060003d5029c7c8e9b8641bf7
George <71167099+letow@users.noreply.github.com>
2023-02-24 13:37:45 +0500

    added i18n decorator for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -2,6 +2,7 @@ import { addDecorator } from "@storybook/react";
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
+import { withI18next } from "../../src/shared/config/storybook/withI18Next/withI18Next";
 import { Theme } from "../../src/app/providers/ThemeProvider";
 
 export const parameters = {
@@ -14,6 +15,7 @@ export const parameters = {
   },
 };
 
+addDecorator(withI18next);
 addDecorator(StyleDecorator);
 addDecorator(RouterDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
---
+++ src/shared/config/storybook/withI18Next/withI18Next.tsx
@@ -0,0 +1,18 @@
+import { Story } from "@storybook/react";
+import { Suspense } from "react";
+import { I18nextProvider } from "react-i18next";
+import { Loader } from "shared/ui/Loader/Loader";
+import i18n from "../../i18n/i18n";
+
+// Wrap your stories in the I18nextProvider component
+export const withI18next = (Story: Story) => {
+  return (
+    // This catches the suspense from components not yet ready (still loading translations)
+    // Alternative: set useSuspense to false on i18next.options.react when initializing i18next
+    <Suspense fallback={<Loader />}>
+      <I18nextProvider i18n={i18n}>
+        <Story />
+      </I18nextProvider>
+    </Suspense>
+  );
+};
